73c0dc19eb515ad8385679c0b5c833b02ed07f02
TrollerZLabZ <69135083+TrollerZLabZ@users.noreply.github.com>
2022-02-06 09:13:28 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+   /**
+    * @title Guild Id
+    * @description The guild id of the server
+    */
+   guildId: "",
+   /**
+    * @title Role Id
+    * @description The role to give the server members who have the phrase
+    */
+   roleId: "",
+   /**
+    * @title Phrase
+    * @description The phrase to trigger the role
+    */
+   phrase: "@ALEXA#0114",
+   /**
+    * @title Channel Id
+    * @description The channel id to send the logs to
+    * Currently in a work in progress
+    */
+   channelId: "",
+}
